cf8aff90cc3315a0cac2a8e1b8936b90cea9eff7
tadea <zikl@spsejecna.cz>
2026-01-04 22:12:12 +0100

    feat: add timetable gui page skeleton

--- app/gui/routes.py
+++ app/gui/routes.py
@@ -9,3 +9,8 @@ templates = Jinja2Templates(directory="app/gui/templates")
 @router.get("/", response_class=HTMLResponse)
 def gui_index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+
+@router.get("/timetable", response_class=HTMLResponse)
+def gui_timetable(request: Request):
+    return templates.TemplateResponse("timetable.html", {"request": request})
